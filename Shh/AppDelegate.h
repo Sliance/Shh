580fff3a1910d93257e35b6ff2c22caebddfc4fa
Slliance <1521161528@qq.com>
2018-08-23 13:42:06 +0800

    Initial Commit

---
+++ Shh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Shh
+//
+//  Created by 燕来秋mac9 on 2018/8/23.
+//  Copyright © 2018年 zhangshu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Shh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Shh
+//
+//  Created by 燕来秋mac9 on 2018/8/23.
+//  Copyright © 2018年 zhangshu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
